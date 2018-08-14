1beeb36206d2d205ead7c30c3c900c87e941cc5d
sangumee <super2451894@gmail.com>
2018-08-14 22:26:41 +0900

    View Webpage Complete
    View Webpage Complete

--- main.js
+++ main.js
@@ -6,20 +6,35 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 
+var db = require('./lib/db');
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+        // fs.readdir('./data', function(error, filelist){
+        //   var title = 'Welcome';
+        //   var description = 'Hello, Node.js';
+        //   var list = template.list(filelist);
+        //   var html = template.HTML(title, list,
+        //     `<h2>${title}</h2>${description}`,
+        //     `<a href="/create">create</a>`
+        //   );
+        //   response.writeHead(200);
+        //   response.end(html);
+        // });
+        db.query(`SELECT * FROM topic`, function(error, topics){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
+                `<h2>${title}</h2>${description}`,
+                `<a href="/create">create</a>`
+              );
+
           response.writeHead(200);
           response.end(html);
         });
